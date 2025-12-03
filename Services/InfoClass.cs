2cb38ba1fe2aaea8bd2b31c06b8e6cc5bbcd45ee
DangerX7 <63203272+DangerX7@users.noreply.github.com>
2025-12-03 14:42:14 +0200

    -update 1.8

--- Services/InfoClass.cs
+++ Services/InfoClass.cs
@@ -8,8 +8,8 @@ namespace SC3_pnach_editor.Services
 {
     public class InfoClass
     {
-        public static string editorVersion = "Version 1.7";
-        public static string editorReleaseDate = "Release Date 27/11/2025";
+        public static string editorVersion = "Version 1.8";
+        public static string editorReleaseDate = "Release Date 3/12/2025";
         public static string discordServer = "https://discord.gg/dSWf2et8g7";
     }
 }
